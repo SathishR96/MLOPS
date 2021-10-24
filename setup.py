139c66a93b9bf19fba3e1955455ff41f2022db72
SathishR96 <sathishprince96@gmail.com>
2021-10-24 15:35:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PCB Board prediction using MLOPS',
+    author='Sathish R',
+    license='MIT',
+)
